5ac96f04314504049b2c2490ae3703866047367d
pa1511 <peafric@gmail.com>
2017-04-09 21:08:05 +0200

    Normalization function fix

--- src/application/data/handling/SymbolDataNormalizer.java
+++ src/application/data/handling/SymbolDataNormalizer.java
@@ -2,21 +2,35 @@ package application.data.handling;
 
 import javax.annotation.Nonnull;
 
-import util.NormalizationUtils;
-
 public class SymbolDataNormalizer {
 
 	private SymbolDataNormalizer() {}
 	
 	private static final int lower = -1;
 	private static final int upper = 1;
-
+	private static final int interval = upper - lower;
+	
 	public static void normalizeSymbolSamples(@Nonnull double[][] samples) {
-		NormalizationUtils.normalize(samples, lower, upper, true);
+		for(double[] sample:samples)
+			normalizeSample(sample);
 	}
 
 	public static void normalizeSample(@Nonnull double[] rawSample) {
-		NormalizationUtils.normalize(rawSample, lower, upper, false);
+		
+		double maxX = Double.MIN_VALUE, minX = Double.MAX_VALUE;
+		double maxY = Double.MIN_VALUE ,minY = Double.MAX_VALUE;
+		
+		for(int i=0; i<rawSample.length; i+=2){
+			maxX = Math.max(maxX, rawSample[i]);
+			minX = Math.min(minX, rawSample[i]);
+			maxY = Math.max(maxY, rawSample[i + 1]);
+			minY = Math.min(minY, rawSample[i + 1]);
+		}
+		
+		for(int i=0; i<rawSample.length; i+=2){
+			rawSample[i] = ((rawSample[i] - minX) / (maxX - minX)) * interval + lower;
+			rawSample[i + 1] = ((rawSample[i + 1] - minY) / (maxY - minY)) * interval + lower;
+		}
 	}
 
 }
